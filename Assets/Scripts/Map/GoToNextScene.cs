bef7f5cb973c8a8ef23ad0c8ec8b3859361b4ee1
s1090349 <s1090349@gm.pu.edu.tw>
2023-08-28 17:15:34 +0800

    2023/08/23合併+場景切換
    2023/08/23合併+場景切換

---
+++ Assets/Scripts/Map/GoToNextScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D other)  //other是指碰撞到的東西 
+    {
+        if (other.gameObject.tag == "tp")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+    }
+}
